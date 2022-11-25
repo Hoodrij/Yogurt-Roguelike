48d51c6e5b4619c1c1d14426e745eaaa5b8f49f5
Slava Korzun <Hoodrij@gmail.com>
2022-11-25 17:16:30 +0200

    Update RunGameJob.cs

--- FlowJob-Roguelike/Assets/Scripts/Roguelike/Jobs/RunGameJob.cs
+++ FlowJob-Roguelike/Assets/Scripts/Roguelike/Jobs/RunGameJob.cs
@@ -15,14 +15,14 @@ namespace Roguelike.Jobs
                 .Add<Data>()
                 .Add<Assets>();
 
-            // while (true)
-            // {
-            // }
-            await new LevelFactoryJob().Run();
-            await new RunTurnJob().Run();
+            while (true)
+            {
+                await new LevelFactoryJob().Run();
+                await new RunTurnJob().Run();
 
-            Entity level = Query.Of<Level>().Single();
-            level.Kill();
+                Entity level = Query.Of<Level>().Single();
+                level.Kill();
+            }
             
             return default;
         }
