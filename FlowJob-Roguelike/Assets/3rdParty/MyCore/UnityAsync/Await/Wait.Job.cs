31e8e917bf857463f79bd55ba11d77a9f2937f7e
Slava Korzun <Hoodrij@gmail.com>
2022-11-26 21:39:26 +0200

    Revert "CustomAsync to UniTask"
    This reverts commit f2a78058f9e107a6d2c8072c5c99288ce18966a6.

---
+++ FlowJob-Roguelike/Assets/3rdParty/MyCore/UnityAsync/Await/Wait.Job.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Core.Tools;
+
+namespace UnityAsync
+{
+    public partial class Wait
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static AwaitInstructionAwaiter<WaitForFrames> WaitUpdate(this ILifetimeOwner lifetimeOwner) 
+			=> Update.ConfigureAwait(lifetimeOwner.Lifetime);
+		
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static AwaitInstructionAwaiter<WaitForFrames> WaitLateUpdate(this ILifetimeOwner lifetimeOwner) 
+			=> LateUpdate.ConfigureAwait(lifetimeOwner.Lifetime);
+		
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static AwaitInstructionAwaiter<WaitForFrames> WaitFixedUpdate(this ILifetimeOwner lifetimeOwner) 
+			=> FixedUpdate.ConfigureAwait(lifetimeOwner.Lifetime);
+		
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static AwaitInstructionAwaiter<WaitForSeconds> WaitSeconds(this ILifetimeOwner lifetimeOwner, float duration) 
+			=> Seconds(duration).ConfigureAwait(lifetimeOwner.Lifetime);
+		
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static AwaitInstructionAwaiter<WaitForSecondsRealtime> WaitSecondsRealtime(this ILifetimeOwner lifetimeOwner, float duration) 
+			=> SecondsRealtime(duration).ConfigureAwait(lifetimeOwner.Lifetime);
+  
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static AwaitInstructionAwaiter<WaitUntil> WaitUntil(this ILifetimeOwner lifetimeOwner, Func<bool> condition) 
+			=> Until(condition).ConfigureAwait(lifetimeOwner.Lifetime);
+		
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static AwaitInstructionAwaiter<WaitWhile> WaitWhile(this ILifetimeOwner lifetimeOwner, Func<bool> condition) 
+			=> While(condition).ConfigureAwait(lifetimeOwner.Lifetime);
+    
+    }
+}
---
+++ FlowJob-Roguelike/Assets/3rdParty/MyCore/UnityAsync/AwaitInstructions/WaitForSecondsRealtime.cs
@@ -0,0 +1,17 @@
+﻿namespace UnityAsync
+{
+	public struct WaitForSecondsRealtime : IAwaitInstruction
+	{
+		readonly float finishTime;
+
+		bool IAwaitInstruction.IsCompleted() => AsyncManager.CurrentUnscaledTime >= finishTime;
+
+		/// <summary>
+		/// Waits for the specified number of (unscaled) seconds to pass before continuing.
+		/// </summary>
+		internal WaitForSecondsRealtime(float seconds)
+		{
+			finishTime = AsyncManager.CurrentUnscaledTime + seconds;
+		}
+	}
+}
