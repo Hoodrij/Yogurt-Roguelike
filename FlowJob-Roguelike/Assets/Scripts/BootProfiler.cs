49882d08bc38b773504bb21937ef3a86d90c942e
Slava Korzun <Hoodrij@gmail.com>
2023-01-22 13:36:00 +0200

    Profiler scene

---
+++ FlowJob-Roguelike/Assets/Scripts/BootProfiler.cs
@@ -0,0 +1,36 @@
+﻿using FlowJob;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+namespace Roguelike
+{
+    public class BootProfiler : MonoBehaviour
+    {
+        private TurnOwner turnOwner;
+        
+        private void Update()
+        {
+            Profiler.BeginSample("---Create");
+            for (int i = 0; i < 1000; i++)
+            {
+                Entity.Create()
+                    .Add(turnOwner);
+            }
+            Profiler.EndSample();
+            
+            Profiler.BeginSample("---Iterate");
+            foreach (Entity entity in Query.Of<TurnOwner>())
+            {
+                TurnOwner turnOwner = entity.Get<TurnOwner>();
+            }
+            Profiler.EndSample();
+            
+            Profiler.BeginSample("---Kill");
+            foreach (Entity entity in Query.Of<TurnOwner>())
+            {
+                entity.Kill();
+            }
+            Profiler.EndSample();
+        }
+    }
+}
