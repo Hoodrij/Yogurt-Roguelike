580d589505a2229c043cfd4f4ddd57bb8f508342
Slava Korzun <Hoodrij@gmail.com>
2023-01-22 13:36:15 +0200

    Boot profiling removed

--- FlowJob-Roguelike/Assets/Scripts/Boot.cs
+++ FlowJob-Roguelike/Assets/Scripts/Boot.cs
@@ -1,41 +1,12 @@
-﻿using FlowJob;
-using UnityEngine;
-using UnityEngine.Profiling;
+﻿using UnityEngine;
 
 namespace Roguelike
 {
     public class Boot : MonoBehaviour
     {
-        private TurnOwner turnOwner;
-        
         private void Awake()
         {
-            // new RunGameJob().Run();
-        }
-
-        private void Update()
-        {
-            Profiler.BeginSample("---Create");
-            for (int i = 0; i < 100; i++)
-            {
-                Entity.Create()
-                    .Add(turnOwner);
-            }
-            Profiler.EndSample();
-            
-            Profiler.BeginSample("---Iterate");
-            foreach (Entity entity in Query.Of<TurnOwner>())
-            {
-                TurnOwner turnOwner = entity.Get<TurnOwner>();
-            }
-            Profiler.EndSample();
-            
-            Profiler.BeginSample("---Kill");
-            foreach (Entity entity in Query.Of<TurnOwner>())
-            {
-                entity.Kill();
-            }
-            Profiler.EndSample();
+            new RunGameJob().Run();
         }
     }
 }
